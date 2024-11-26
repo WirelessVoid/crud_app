d8ece1b3e8bc64c909cb116092d9a05ed7e2eee7
WirelessVoid <benjiv2015@gmail.com>
2024-11-26 09:32:40 -0300

    Cambios en configuración

---
+++ src/model/producto.ts
@@ -0,0 +1,11 @@
+export class ProductoModel{
+    id:number;
+    descripcion:string;
+    precio:number;
+
+    constructor(){
+        this.id=1;
+        this.descripcion="";
+        this.precio=0;
+    }
+}
